4b7d973c6f8698274e552522ce8a2aaa7c3dd72c
Furkan khan <56495312+Mr667@users.noreply.github.com>
2025-12-26 19:44:25 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,36 @@
+import random
+'''
+1 for sake
+-1 for water
+0 for gun
+'''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "water", 0: "Gun"}
+
+you = youDict[youstr]
+
+# By now we have 2 numbers (variables), you and coumputer
+
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("It's a Draw")
+else:
+    if(computer == -1 and you == 1):
+        print("You Win!")
+    elif(computer == -1 and you == 0):
+        print("You Lose!")
+    elif(computer == 1 and you == -1):
+        print("You Lose!")
+    elif(computer == 1 and you == 0):
+        print("You Win!")
+    elif(computer == 0 and you == -1):
+        print("You Win!")
+    elif(computer == 0 and you == 1):
+        print("You Lose!")
+
+    else:
+        print("Something is Wrong!")
